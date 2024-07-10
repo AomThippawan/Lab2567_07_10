363c271e8a6a74acab81133cba668e3bac3f310d
AomThippawan <aomthippawan1121@gmail.com>
2024-07-10 11:57:08 +0700

    first commit

---
+++ db.config.js
@@ -0,0 +1,9 @@
+//module export เป็นคำสั่งที่ทำให้ไฟล์อื่นสามารถเรียกใช้ตัวแปรจากไฟลนี้ได้
+//this file ตั้งค่าตัวแปร
+module.exports = {
+    HOST : 'localhost',
+    USER : 'root',
+    DATABASE : '652021052DB',
+    PASSWORD : '',
+    PORT : '3306'
+};
